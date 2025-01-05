c04ba325e60daa6acf9a5d0791963edf9aece243
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-01-05 16:00:09 +0800

    Update lean1.sh

--- diy/lean/lean1.sh
+++ diy/lean/lean1.sh
@@ -3,7 +3,15 @@
 # DaoDao's script
 #=================================================
 ##添加自己的插件库
+#rm -rf ./package/lean/k3screenctrl
+               
+#sed -i "1isrc-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk\n" feeds.conf.default
+
+##添加自己的插件库
 rm -rf ./package/lean/k3screenctrl
+rm -rf ./package/lean/autocore
                
 sed -i "1isrc-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk\n" feeds.conf.default
 
+sed -i 's/#src-git luci/src-git luci/g' feeds.conf.default
+sed -i '/luci.git;openwrt-23.05/d' feeds.conf.default
