ff65cb18fc31854fbe8edd5b8c788fb7de664ee5
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-01-06 22:56:04 +0800

    Update lean1.sh

--- diy/lean/lean1.sh
+++ diy/lean/lean1.sh
@@ -5,5 +5,6 @@
 ##添加自己的插件库
 rm -rf ./package/lean/k3screenctrl
                
-sed -i "1isrc-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk\n" feeds.conf.default
+sed -i "1isrc-git 2305ipk https://github.com/sos801107/2305-ipk\n" feeds.conf.default
 
+#sed -i "1isrc-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk\n" feeds.conf.default
