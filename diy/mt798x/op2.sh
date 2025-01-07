0906ed2d2de51d4bbaf2484fce8f8a7a075704ce
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-01-07 21:22:31 +0800

    Update op2.sh

--- diy/mt798x/op2.sh
+++ diy/mt798x/op2.sh
@@ -15,7 +15,7 @@ echo -e "msgstr \"网络存储\"" >> feeds/luci/modules/luci-base/po/zh_Hans/bas
 #echo -e "msgstr \"魔法网络\"" >> feeds/luci/modules/luci-base/po/zh_Hans/base.po
 
 ##配置IP
-sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 ##
 rm -rf ./feeds/extraipk/theme/luci-theme-argon-18.06
