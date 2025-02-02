22a2336b69474582c7b40800b53777ec5c8aff3f
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-02-02 16:53:24 +0800

    Update lean1.sh

--- diy/lean/lean1.sh
+++ diy/lean/lean1.sh
@@ -5,8 +5,11 @@
 ##添加自己的插件库
 rm -rf ./package/lean/k3screenctrl
                
-sed -i "1isrc-git 2305ipk https://github.com/sos801107/2305-ipk\n" feeds.conf.default
-
+#sed -i "1isrc-git 2305ipk https://github.com/sos801107/2305-ipk\n" feeds.conf.default
+#rm -rf ./package/lean/k3screenctrl
+rm -rf ./package/lean/autocore
+               
+sed -i "1isrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk\n" feeds.conf.default
 #sed -i 's/openwrt\/luci\/luci.git;openwrt-24.10/g' ./feeds.conf.default
 sed -i '/luci.git;openwrt-24.10/d' ./feeds.conf.default
 sed -i 's/coolsnowwolf\/luci/openwrt\/luci/g' ./feeds.conf.default
