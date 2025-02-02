e2b7d663c64db9bd0b03664111a42475afab98ea
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-02-02 11:16:39 +0800

    Update lean1.sh

--- diy/lean/lean1.sh
+++ diy/lean/lean1.sh
@@ -7,4 +7,7 @@ rm -rf ./package/lean/k3screenctrl
                
 sed -i "1isrc-git 2305ipk https://github.com/sos801107/2305-ipk\n" feeds.conf.default
 
-sed -i 's/openwrt\/luci\/luci.git;openwrt-24.10/g' ./feeds.conf.default
+#sed -i 's/openwrt\/luci\/luci.git;openwrt-24.10/g' ./feeds.conf.default
+sed -i '/luci.git;openwrt-24.10/d' ./feeds.conf.default
+sed -i 's/coolsnowwolf\/luci/openwrt\/luci\/luci.git;openwrt-24.10/g' ./feeds.conf.default
+#sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-bypass/luasrc/controller/*.lua
